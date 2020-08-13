877070fd2ea09d68faa27feb9d6123ba5ce8139e
Md. Omar Faruk <iamomar022@gmail.com>
2020-08-13 08:41:56 +0600

    Changed DB

--- webapp/settings.py
+++ webapp/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'webapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -95,15 +95,15 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
 '''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-'''
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators

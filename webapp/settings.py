87c6dd1164eef466f07d670d15ac00ee7eb88f34
Gift Balogun <blgnbalogun53@gmail.com>
2020-08-17 16:56:34 +0100

    make it better

--- webapp/settings.py
+++ webapp/settings.py
@@ -59,6 +59,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'webapp.urls'
@@ -183,4 +184,4 @@ ACCOUNT_DEFAULT_HTTP_PROTOCOL = "http"
 PaystackConfig.SECRET_KEY = 'sk_test_f7e947052743b341da31891f7565b11d53f1ac37'
 PaystackConfig.PUBLIC_KEY = 'pk_test_9f333bd69a6027cc33c3bd7b6b334ffd179daa87'
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
